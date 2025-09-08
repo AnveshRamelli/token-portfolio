9a00c0fa1e235226542945aaab2e42fa19fba9f4
Anvesh Ramelli <anveshramelli1999@gmail.com>
2025-09-08 10:15:12 +0530

    handled no data state

--- src/components/PortfolioChart.tsx
+++ src/components/PortfolioChart.tsx
@@ -8,36 +8,40 @@ const PortfolioChart = () => {
   return (
     <div>
       <p className="text-muted text-sm">Portfolio Total</p>
-      <div className="flex flex-col sm:flex-row">
-        <div className="flex justify-center">
-          <PieChart width={180} height={180}>
-            <Pie
-              data={chartData}
-              innerRadius={40}
-              outerRadius={80}
-              dataKey="percentage"
-            >
-              {chartData.map((item) => (
-                <Cell key={item.id} fill={item.color} />
-              ))}
-            </Pie>
-          </PieChart>
+      {chartData.length ? (
+        <div className="flex flex-col sm:flex-row">
+          <div className="flex justify-center">
+            <PieChart width={180} height={180}>
+              <Pie
+                data={chartData}
+                innerRadius={40}
+                outerRadius={80}
+                dataKey="percentage"
+              >
+                {chartData.map((item) => (
+                  <Cell key={item.id} fill={item.color} />
+                ))}
+              </Pie>
+            </PieChart>
+          </div>
+          <div className="flex-1 mt-4 sm:mt-0 sm:ml-4 chart-legend h-50 overflow-y-auto">
+            {chartData.map((item) => (
+              <div key={item.id} className="flex justify-between space-y-1">
+                <p className="text-sm" style={{ color: item.color }}>
+                  {item.name} ({item.symbol.toUpperCase()})
+                </p>
+                <p className="text-sm text-muted">
+                  {item.percentage.toFixed(1)}%
+                </p>
+              </div>
+            ))}
+          </div>
         </div>
-        <div className="flex-1 mt-4 sm:mt-0 sm:ml-4 chart-legend h-50 overflow-y-auto">
-          {chartData.map((item) => (
-            <div key={item.id} className="flex justify-between space-y-1">
-              <p className="text-sm" style={{ color: item.color }}>
-                {item.name} ({item.symbol.toUpperCase()})
-              </p>
-              <p className="text-sm text-muted">
-                {item.percentage.toFixed(1)}%
-              </p>
-            </div>
-          ))}
-        </div>
-      </div>
+      ) : (
+        <div className="flex justify-center items-center min-h-30 text-muted">No data</div>
+      )}
     </div>
   );
-};
+};  
 
 export default PortfolioChart;
